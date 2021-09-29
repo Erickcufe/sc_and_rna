0e42d12907717e29b002f046ea4c45e52c8240f7
Erickcufe <erickcufe94@gmail.com>
2021-09-28 23:10:19 -0500

    ...

--- R/28-09-2020_modelo_LOAD_EUR.R
+++ R/28-09-2020_modelo_LOAD_EUR.R
@@ -1,3 +1,7 @@
 install.packages("furrr")
 devtools::install_github("Erickcufe/seekerBio")
 devtools::install_github("Erickcufe/hgdp", ref = "erick")
+
+usethis::git_sitrep()
+usethis::create_github_token()
+gitcreds::gitcreds_set()
